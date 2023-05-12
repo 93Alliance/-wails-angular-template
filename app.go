d79f1a2e26c02228ba4fd60aa9c3df10fa18c814
vincent <fei.wang@sci-jet.com>
2023-05-12 20:29:21 +0800

    add main.go

--- app.go
+++ app.go
@@ -31,6 +31,16 @@ func (b *App) shutdown(ctx context.Context) {
 	// Perform your teardown here
 }
 
+// beforeClose is called when the application is about to quit,
+// either by clicking the window close button or calling runtime.Quit.
+// Returning true will cause the application to continue,
+// false will continue shutdown as normal.
+// beforeClose在单击窗口关闭按钮或调用runtime.Quit即将退出应用程序时被调用.
+// 返回 true 将导致应用程序继续，false 将继续正常关闭。
+func (a *App) beforeClose(ctx context.Context) (prevent bool) {
+	return false
+}
+
 // Greet returns a greeting for the given name
 func (b *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
